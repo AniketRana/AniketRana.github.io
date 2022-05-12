4f4ce16ae59931c05658b90cdf032e4964bf5386
Aniket Rana <ranaannu93@gmail.com>
2022-05-12 16:07:41 -0400

    commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Developer","Full Stack Developer",".Net Developer","Product Manager","Designer"],
+    typeSpeed:100,
+    BackSpeed:20,
+    loop:true
+})
